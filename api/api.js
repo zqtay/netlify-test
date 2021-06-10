109aab053fd6c4b7bdd9247bfa240eee5bfc78ae
zqtay <zqtay96@gmail.com>
2021-06-10 12:42:02 +0800

    api fix

--- api/api.js
+++ api/api.js
@@ -3,12 +3,21 @@ const serverless = require('serverless-http');
 const app = express();
 
 const bodyParser = require('body-parser');
+const router = express.Router();
 
-
-app.use(bodyParser);
-app.get('/', (req, res) => {
-  res.send("Hello world");
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write("<h1>Hello world</h1>");
+  res.end();
 });
+
+router.get('/2/', (req, res) => {
+	res.send("Hi 2");
+});
+
+
+app.use(bodyParser.json());
+app.use('/.netlify/api/api', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
