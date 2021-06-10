728c52bfbf192e1a7c4577097fefef3281cee15c
zqtay <zqtay96@gmail.com>
2021-06-10 12:26:38 +0800

    first commit

---
+++ api/api.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser);
+app.get('/', (req, res) => {
+  res.send("Hello world");
+});
+
+module.exports.handler = serverless(app);
---
+++ app.js
@@ -0,0 +1,3 @@
+const app = require('./api/api');
+
+app.listen(3000, () => console.log('Local app listening on port 3000!'));
